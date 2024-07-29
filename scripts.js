74d530bbb4779174e32dd6e4703888d34612d33b
cbaaabcd <160158095+cbaaabcd@users.noreply.github.com>
2024-07-29 21:48:07 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('surprise-button');
+    
+    button.addEventListener('click', () => {
+        alert('I love you more every day!');
+    });
+});
